4c87aa2bd59c9da7907135b0697d4b7a07f08f41
dandawalat-lh <dandawalat@gmail.com>
2026-08-26 15:57:53 -0400

    Update page.tsx

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
